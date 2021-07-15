1762eec1d05b92025bd5c1e6659af253c354e137
mil29 <74261419+mil29@users.noreply.github.com>
2021-07-15 16:34:38 +0100

    update

--- comparer/static/js/custom.js
+++ comparer/static/js/custom.js
@@ -27,9 +27,3 @@ $(document).ready(function(){
            })
 })
 
-window.addEventListener('beforeunload', function (e) {
-  // Cancel the event
-  e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
-  // Chrome requires returnValue to be set
-  e.returnValue = '';
-});
