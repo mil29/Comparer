e6a6cafed8aabca8a531c225969f608e4b8eb4ac
mil29 <74261419+mil29@users.noreply.github.com>
2021-07-15 19:49:53 +0100

    update

--- comparer/static/js/custom.js
+++ comparer/static/js/custom.js
@@ -1,27 +1,23 @@
 $(document).ready(function(){
 
-
     $("button.add_item").on("click",function(){
 
     let item_count = $('.container .compare_card').length;
 
-    $(".col:last").after(
+    $(".row:last").after(
         `
+        <div class="row form_row">
             <div class="col mb-4">
-                <div class="card compare_card">
-                    <div class=" card-body compare_form">
-                        <h5>Item ${item_count+1}</h5>
-                        <form action="{% url 'compare_ingredients' %}" method="post" id="compare">
-                            <input id="token" type="hidden" value="{% csrf_token %}" ></input>
-                            <textarea name="ingredients" id="ingredients${item_count+1}" class="all_textareas"></textarea>
-                            <br>
-                            <button type="submit" class="btn btn-info">Submit</button>
-                        </form>
-                        
+                    <h5>Item</h5>
+                <div class="card form_card">
+                    <div class="card-body compare_cardbody">
+
+                        <textarea name="ingredients" id="" cols="30" rows="10"></textarea>
                         
                     </div>
-                </div>
+                </div>    
             </div>
+        </div>
         `
             );
            })
