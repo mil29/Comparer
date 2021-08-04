72668da3434167f097d6dca3888686244a952bf9
mil29 <74261419+mil29@users.noreply.github.com>
2021-08-04 10:59:39 +0100

    up

--- comparer/comparer/settings.py
+++ comparer/comparer/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&dapocp0en(j%(a(hp%ep3n&c-5yif21pn#!xv*ur-yf+3q3bh'
+
+# SECRET_KEY = 'django-insecure-&dapocp0en(j%(a(hp%ep3n&c-5yif21pn#!xv*ur-yf+3q3bh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
